6fadfd728e1767e13ea046d7f1caf1e15c247e82
Galina Slavova <galina@crafity.com>
2012-07-15 20:51:28 +0200

    Refactored the config file

--- config.js
+++ config.js
@@ -48,29 +48,29 @@ exports.open = function (path, info, callback) {
 	if (configurations[path]) { return callback(null, configurations[path]); }
 
 	fs.readFile(path, function (err, data) {
-		if (err) { callback(err); } else {
-			var config = JSON.parse(data.toString())
-				, environment = process.env.NODE_ENV || config.environment
-				, result
-				;
+		if (err) { return callback(err); }
+		var config = JSON.parse(data.toString())
+			, environment = process.env.NODE_ENV || config.environment
+			, result
+			;
 
-			if (!environment) {
-				return callback(new Error("No configuration environment is set in the configuration file or NODE_ENV."));
-			} else if (!config[environment]) {
-				return callback(new Error("Environment '" + environment + "' is not defined in the configuration."));
-			}
-
-			if (info) {
-				console.log("crafity-configuration:", "Selecting environment setting '" +
-					environment + "' as configured in '" +
-					(process.env.NODE_ENV ? "NODE_ENV" : "Configuration file") + "'");
-			}
-
-			result = objects.merge(config.shared, config[environment]) || {};
-			result.environment = environment;
-			configurations[path] = result;
-			callback(null, result);
+		if (!environment) {
+			return callback(new Error("No configuration environment is set in the configuration file or NODE_ENV."));
+		} else if (!config[environment]) {
+			return callback(new Error("Environment '" + environment + "' is not defined in the configuration."));
 		}
+
+		if (info) {
+			console.log("crafity-configuration:", "Selecting environment setting '" +
+				environment + "' as configured in '" +
+				(process.env.NODE_ENV ? "NODE_ENV" : "Configuration file") + "'");
+		}
+
+		result = objects.merge(config.shared, config[environment]) || {};
+		result.environment = environment;
+		configurations[path] = result;
+		return callback(null, result);
+
 	});
 
 };
